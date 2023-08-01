6891318488020c00de0ba8dfb867e2cdf74ee168
Radin <110737910+radG1t@users.noreply.github.com>
2023-08-01 20:57:58 +0330

    Update bot.php
    hajm koli pak shod

--- bot.php
+++ bot.php
@@ -4928,10 +4928,6 @@ if($userInfo['step'] == "showAccount" and $text != $cancelText){
                     ['text'=>"√ دانلود √",'callback_data'=>"wizwizch"],
                     ],
                 [
-                    ['text'=>$total??" ",'callback_data'=>"wizwizch"],
-                    ['text'=>"† حجم کلی †",'callback_data'=>"wizwizch"],
-                    ],
-                [
                     ['text'=>$leftMb??" ",'callback_data'=>"wizwizch"],
                     ['text'=>"~ حجم باقیمانده ~",'callback_data'=>"wizwizch"],
                     ],
